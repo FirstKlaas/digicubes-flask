e866a1bbe7bf40182bef77f3f6366ec2de843c0a
Klaas Nebuhr <nebuhr@encoway.de>
2020-05-02 20:08:12 +0200

    Not using flask session any more

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,7 @@
-from flask import Flask, session
-#from flask_session import Session
-
 from digicubes_flask.web import create_app
-
 
 app = create_app()
 
-#app.config["SESSION_TYPE"] = 'memcached'
-#Session(app)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(port=5000)
 
