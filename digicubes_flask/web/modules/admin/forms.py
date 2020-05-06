3c331f4863260a8c19875bb3fe60110458209c0d
Klaas Nebuhr <nebuhr@encoway.de>
2020-05-06 12:56:08 +0200

    Added validations to the forms

--- digicubes_flask/web/modules/admin/forms.py
+++ digicubes_flask/web/modules/admin/forms.py
@@ -39,16 +39,24 @@ class UserForm(FlaskForm):
     users.
     """
 
-    first_name = StringField("First Name", widget=w.materialize_input)
-    last_name = StringField("Last Name", widget=w.materialize_input)
+    first_name = StringField(
+        "First Name", widget=w.materialize_input, validators=[validators.InputRequired()]
+    )
+
+    last_name = StringField(
+        "Last Name", widget=w.materialize_input, validators=[validators.InputRequired()]
+    )
+
     email = StringField(
         "Email",
         widget=w.materialize_input,
         validators=[validators.Email(), validators.InputRequired()],
     )
+
     login = StringField(
         "The Account Name", widget=w.materialize_input, validators=[validators.InputRequired()]
     )
+
     password = PasswordField("Password", widget=w.materialize_password)
     is_active = BooleanField("Active", widget=w.materialize_checkbox)
     is_verified = BooleanField("Verified", widget=w.materialize_checkbox)
@@ -61,12 +69,11 @@ class UserForm(FlaskForm):
         """
         try:
             digicubes.user.get_by_login(digicubes.token, field.data)
-            # If we can find an account, we raise the ValidatioNerror to 
+            # If we can find an account, we raise the ValidatioNerror to
             # signal, that this account is not available
             raise ValidationError("Account already exists")
         except ex.DoesNotExist:
-            pass # If we can not find the account, that's perfect.
-            
+            pass  # If we can not find the account, that's perfect.
 
 
 class SchoolForm(FlaskForm):
@@ -74,9 +81,23 @@ class SchoolForm(FlaskForm):
     Create school form
     """
 
-    name = StringField("Name", widget=w.materialize_input)
-    description = TextAreaField("Description", widget=w.materialize_textarea)
+    name = StringField("Name", widget=w.materialize_input, validators=[validators.InputRequired()])
+    description = TextAreaField(
+        "Description", widget=w.materialize_textarea, validators=[validators.InputRequired()]
+    )
     submit = SubmitField("Ok", widget=w.materialize_submit)
+
+    def validate_name(self, field):
+        """
+        Checks, if the school already exists, as the name has to be unique
+        """
+        try:
+            digicubes.school.get_by_name(digicubes.token, field.data)
+            # If we can find an account, we raise the ValidatioNerror to
+            # signal, that this account is not available
+            raise ValidationError("School already exists")
+        except ex.DoesNotExist:
+            pass  # If we can not find the account, that's perfect.
 
 
 class CourseForm(FlaskForm):
@@ -87,12 +108,18 @@ class CourseForm(FlaskForm):
     school_id = HiddenField()
     name = StringField("Name", widget=w.materialize_input, validators=[validators.InputRequired()])
 
-    description = TextAreaField("Description", widget=w.materialize_textarea)
+    description = TextAreaField(
+        "Description", widget=w.materialize_textarea, validators=[validators.InputRequired()]
+    )
+
     from_date = DateField(
         "Starting from", default=date.today(), format="%d.%m.%Y", widget=w.materialize_picker
     )
+
     until_date = DateField(
         "Ending at", default=date.today(), format="%d.%m.%Y", widget=w.materialize_picker
     )
+
     is_private = BooleanField("Private", widget=w.materialize_switch)
+
     submit = SubmitField("Ok", widget=w.materialize_submit)
