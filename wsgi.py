70f665b4aeba3b15e941ca7be69877ed44672225
Klaas Nebuhr <nebuhr@encoway.de>
2021-03-24 19:42:38 +0100

    Debug configuration changed

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,8 @@
-import sys
-import os
-sys.path.append("/digicubes")
-
 from digicubes_flask.web import create_app
 
 app = create_app()
 
 # gunicorn -b 0.0.0.0:5000 --workers=2 --threads=4 --worker-class=gthread wsgi:app
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0")
