3874ad0d7f1080e8dcca018aebf8496e06b419c8
Klaas Nebuhr <nebuhr@encoway.de>
2021-03-26 07:20:05 +0100

    INfo links as macro fur reusability

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-from digicubes_flask.web import create_app
+from digicubes_flask.web import create_app as dc_create_app
 
-app = create_app()
+app = dc_create_app()
 
 # gunicorn -b 0.0.0.0:5000 --workers=2 --threads=4 --worker-class=gthread wsgi:app
 
-#if __name__ == "__main__":
-#    app.run(debug=True, host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5555)
